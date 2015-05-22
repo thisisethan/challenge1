82c2d104602f2baf41a03a5f42a7b57e4c336307
Ethan McCue <emccue@live.com>
2015-05-21 23:25:16 -0400

    adding config

---
+++ config.h
@@ -0,0 +1,2 @@
+#define left_motor_port 0
+#define right_motor_port 3
--- movement.c
+++ movement.c
@@ -1,36 +1,38 @@
 #include "movement.h"
+#include "config.h"
 #include <time.h>
+
 
 void driveForward(int power)
 {
-	motor(0, power);
-	motor(3, power);
+	motor(left_motor_port, power);
+	motor(right_motor_port, power);
 }
 
 void turnLeft(int power)
 {
-	motor(0, power);
-	motor(3, -power);
+	motor(left_motor_port, power);
+	motor(right_motor_port, -power);
 }
 
 
 void turnRight(int power)
 {
-	motor(0, -power);
-	motor(3, power);
+	motor(left_motor_port, -power);
+	motor(right_motor_port, power);
 }
 
 void driveBackward(int power)
 {
-	motor(0, -power);
-	motor(3, -power);
+	motor(left_motor_port, -power);
+	motor(right_motor_port, -power);
 }
 
 void motorStop(void)
 
 { 
-	motor(0, 0);
-	motor(3, 0);
+	motor(left_motor_port, 0);
+	motor(right_motor_port, 0);
 	
 }
 
@@ -43,15 +45,15 @@ void lineFollow(double time)
 	{
 		if (analog(6) < 900)
 		{
-			motor(0, 20);
-			motor(3, 100);
+			motor(left_motor_port, 20);
+			motor(right_motor_port, 100);
 			
 		}
 		
 		else	
 		{
-				motor(0, 100);
-				motor(3, 20);
+				motor(left_motor_port, 100);
+				motor(right_motor_port, 20);
 		}
 		diff = clock() - start;
 		sec = diff * 10 / CLOCKS_PER_SEC;
